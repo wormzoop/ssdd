6244e93f53198bd08944bb853ffd181c6c5a1bef
wormzoop <446706489@qq.com>
2017-04-04 16:38:59 +0800

    eureka

--- cloud-client-simple/src/main/java/com/zoop/CloudClientSimpleApplication.java
+++ cloud-client-simple/src/main/java/com/zoop/CloudClientSimpleApplication.java
@@ -2,8 +2,10 @@ package com.zoop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudClientSimpleApplication {
 
 	public static void main(String[] args) {
