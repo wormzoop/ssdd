c0e23f6aa289ce98504fec0a9afde2f03f20b36e
wormzoop <446706489@qq.com>
2017-08-01 16:46:27 +0800

    1

--- boot-mybatis-annotation/src/main/java/com/zoop/BootMybatisAnnotationApplication.java
+++ boot-mybatis-annotation/src/main/java/com/zoop/BootMybatisAnnotationApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan("com.zoop")
 @MapperScan(basePackages = "com.zoop.mapper")
 public class BootMybatisAnnotationApplication {
