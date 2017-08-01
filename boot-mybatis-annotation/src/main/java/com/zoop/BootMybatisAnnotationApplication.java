9cc211a2f488c3ad088dd0f23fd9dec2de97f4a4
wormzoop <446706489@qq.com>
2017-08-01 17:50:33 +0800

    eureka

--- boot-mybatis-annotation/src/main/java/com/zoop/BootMybatisAnnotationApplication.java
+++ boot-mybatis-annotation/src/main/java/com/zoop/BootMybatisAnnotationApplication.java
@@ -7,9 +7,11 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
 @ComponentScan("com.zoop")
 @MapperScan(basePackages = "com.zoop.mapper")
 public class BootMybatisAnnotationApplication {
--- cloud-client-simple/src/main/java/com/zoop/CloudClientSimpleApplication.java
+++ cloud-client-simple/src/main/java/com/zoop/CloudClientSimpleApplication.java
@@ -3,9 +3,11 @@ package com.zoop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class CloudClientSimpleApplication {
 
 	public static void main(String[] args) {
