1c976213cf8c60f6af71507ca916d23a92ea6d90
wormzoop <446706489@qq.com>
2018-04-10 11:28:30 +0800

    jpa

---
+++ boot-mybatis-jpa/src/main/java/com/zoop/BootMybatisJpaApplication.java
@@ -0,0 +1,12 @@
+package com.zoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMybatisJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMybatisJpaApplication.class, args);
+	}
+}
---
+++ boot-mybatis-jpa/src/test/java/com/example/bootmybatisjpa/BootMybatisJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.bootmybatisjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootMybatisJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
